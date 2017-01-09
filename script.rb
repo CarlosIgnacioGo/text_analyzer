0b235f2e541a8053f029996bc1eb59fbf4699b54
Carlos Gomez <carlos221519@gmail.com>
2017-01-09 15:39:28 -0300

    [uno-nueve] gsub

--- script.rb
+++ script.rb
@@ -2,7 +2,7 @@ puts 'Ingresa el texto a analisar'
 text = gets.chomp
 
 
-replacements_words = [ ["uno", "1"],["dos", "2"],["tres", "3"],["cuatro", "4"],["cinco", "5"],["sies", "6"],["siete", "7"],["ocho", "8"],["nueve", "9"]]
+replacements_words = [ [" ", ""],["uno", "1"],["dos", "2"],["tres", "3"],["cuatro", "4"],["cinco", "5"],["sies", "6"],["siete", "7"],["ocho", "8"],["nueve", "9"]]
 
 text.downcase! 
 
@@ -14,9 +14,10 @@ def text_valid?(text)
 	valid_phone = /(9[6-9][0-9]{7})/.match(text)
 	valid_telefone_sg = /([2-9][0-9]{8})/.match(text)
 	valid_telefone_cl = /([3-7][1-9][2-9][0-9]{5,6})/.match(text)
-	valid_correo = /\S+@\S+\.\S+/.match(text)
+	valid_email = /\S+@\S+\.\S+/.match(text)
+	valid_email_for_words = /[Gg]mail|[Hh]otmail/.match(text)
 
-	if valid_phone || valid_telefone_sg ||  valid_telefone_cl || valid_correo
+	if valid_phone || valid_telefone_sg ||  valid_telefone_cl || valid_email || valid_email_for_words
 		puts 'false'
 	else
 	  puts 'true'
